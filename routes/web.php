29b0d85408d07660d8fc29bb5fa8a33b1eeb111e
Davide Pulci <davide.pulci@gmail.com>
2022-05-30 17:59:14 +0200

    definite le rotte admin + guest

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')
+        ->name('home');
+    });
+
+Route::get("{any?}", function(){
+    return view('guest.home');
+})->where("any", ".*");
+
+
