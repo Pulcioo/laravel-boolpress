5a366ee1b94bfa6861fead6446c0a6ec6413fab5
Davide Pulci <davide.pulci@gmail.com>
2022-06-06 17:25:41 +0200

    aggiunto seeder tabella tags

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tag;
+
+use Illuminate\Support\Str;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $tags = ['Gaming', 'Nerd', 'Routine'];
+
+        foreach($tags as $tag) {
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($tag);
+            $new_tag->save();
+        }
+    }
+}
